d4800de6b2262a30eb085fd8cec638e2f03e2d08
Alexandre Chêne <kooparse@protonmail.com>
2018-05-11 13:20:32 +0200

    Camera moving with cursor position

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ extern crate nalgebra as na;
 mod renderer;
 mod utils;
 
-use glutin::{ElementState, VirtualKeyCode};
+use glutin::{ElementState, MouseCursor, VirtualKeyCode};
 use std::str;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 use glutin::{EventsLoop, GlContext, GlWindow};
 use na::{Isometry3, Matrix4, Perspective3, Point3, Vector3};
 use glutin::Event::WindowEvent;
-use glutin::WindowEvent::{Closed, KeyboardInput, Resized};
+use glutin::WindowEvent::{CloseRequested, CursorMoved, KeyboardInput, Resized};
 
 use renderer::Pipeline;
 
@@ -31,9 +31,11 @@ fn main() {
         .with_title(TITLE)
         .with_dimensions(WINDOW_WIDTH as u32, WINDOW_HEIGT as u32);
 
+    // gl_window..set_mouse_position(last_cursor_pos_x, last_cursor_pos_y);
     let context = glutin::ContextBuilder::new().with_vsync(true);
 
     let gl_window = GlWindow::new(window, context, &window_loop).unwrap();
+
     //
     // Set current context
     unsafe { gl_window.make_current().unwrap() }
@@ -80,12 +82,28 @@ fn main() {
     let start = Instant::now();
     let mut running = true;
 
+    // Intialize cursor position to be at
+    // the center of the screen
+    let mut last_cursor_pos_x: f32 = WINDOW_WIDTH / 2.;
+    let mut last_cursor_pos_y: f32 = WINDOW_HEIGT / 2.;
+    let mut first_cursor_move: bool = true;
+    let mut yaw: f32 = -90.;
+    let mut pitch: f32 = 0.;
+
     let mut camera_pos = Vector3::new(0., 0., 3.);
-    let camera_front = Vector3::new(0., 0., -1.);
+    let mut camera_front = Vector3::new(0., 0., -1.);
     let camera_up = Vector3::new(0., 1., 0.);
 
     let mut dt: f32;
     let mut last_frame: f32 = 0.;
+
+    gl_window
+        .window()
+        .set_cursor_position(last_cursor_pos_x as i32, last_cursor_pos_y as i32)
+        .expect("Failed to set cursor position at the center");
+
+    // Hide cursor
+    gl_window.window().set_cursor(MouseCursor::NoneCursor);
 
     while running {
         let current_frame = utils::duration_to_secs(start.elapsed()) as f32;
@@ -97,6 +115,43 @@ fn main() {
         window_loop.poll_events(|e| {
             if let WindowEvent { event, .. } = e {
                 match event {
+                    CursorMoved {
+                        position: (pos_x, pos_y),
+                        // modifiers,
+                        ..
+                    } => {
+                        let (pos_x, pos_y) = (pos_x as f32, pos_y as f32);
+                        if first_cursor_move {
+                            last_cursor_pos_x = pos_x;
+                            last_cursor_pos_y = pos_y;
+                            first_cursor_move = false;
+                        }
+
+                        let mut x_offset = pos_x - last_cursor_pos_x;
+                        let mut y_offset = last_cursor_pos_y - pos_y;
+                        last_cursor_pos_x = pos_x;
+                        last_cursor_pos_y = pos_y;
+
+                        let sensibility = 0.05;
+                        x_offset *= sensibility;
+                        y_offset *= sensibility;
+
+                        yaw += x_offset;
+                        pitch += y_offset;
+
+                        if pitch > 89. {
+                            pitch = 89.
+                        }
+                        if pitch < -89. {
+                            pitch = -89.
+                        }
+
+                        camera_front = Vector3::new(
+                            yaw.to_radians().cos() * pitch.to_radians().cos(),
+                            pitch.to_radians().sin(),
+                            yaw.to_radians().sin() * pitch.to_radians().cos(),
+                        ).normalize();
+                    }
                     KeyboardInput {
                         input:
                             glutin::KeyboardInput {
@@ -136,7 +191,7 @@ fn main() {
                         Some(VirtualKeyCode::Escape) => running = false,
                         _ => (),
                     },
-                    Closed => running = false,
+                    CloseRequested => running = false,
                     Resized(w, h) => gl_window.resize(w, h),
                     _ => (),
                 }
