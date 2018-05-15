a9ebb353b997331f7cc9e7c69fd099bdc2132b02
Alexandre Chêne <kooparse@protonmail.com>
2018-05-15 14:54:22 +0200

    Able to hide texture from object

--- src/renderer/pipeline.rs
+++ src/renderer/pipeline.rs
@@ -9,6 +9,7 @@ pub struct Pipeline<'a> {
     pub config: RendererConfig<'a>,
     pub texture: Texture,
     pub shader: Shader,
+    show_texture: bool,
 }
 
 impl<'a> Pipeline<'a> {
@@ -27,13 +28,28 @@ impl<'a> Pipeline<'a> {
             config,
             texture,
             shader,
+            show_texture: true,
         }
+    }
+
+    pub fn set_texture_off(&mut self) {
+        self.show_texture = false
+    }
+
+    pub fn set_texture_true(&mut self) {
+        self.show_texture = true
     }
 
     // Draw our stuff
     pub fn render(&mut self) {
+        let mut texture_id: u32 = 0;
+
+        if self.show_texture {
+            texture_id = self.config.texture_id;
+        };
+
         unsafe {
-            gl::BindTexture(gl::TEXTURE_2D, self.texture.id);
+            gl::BindTexture(gl::TEXTURE_2D, texture_id);
             gl::BindVertexArray(self.config.vao);
             gl::DrawArrays(gl::TRIANGLES, 0, 36);
         }
